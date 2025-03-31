9dd923148e85897d970bfd87a41bb8a0103715b5
SeryiBaran <seryibaranminepy@yandex.ru>
2025-04-01 02:33:29 +0300

    upd

--- components.d.ts
+++ components.d.ts
@@ -8,7 +8,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    OnAir: typeof import('./src/components/OnAir.vue')['default']
     Programs: typeof import('./src/components/Programs.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
