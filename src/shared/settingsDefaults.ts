03e2ae4cf9e9edc39989f9f29c7af51dd2de6b74
SeryiBaran <seryibaranminepy@yandex.ru>
2025-04-14 16:15:06 +0300

    hey git you forget this file!

---
+++ src/shared/settingsDefaults.ts
@@ -0,0 +1,57 @@
+import type { GenreID, UnoCSSColorName } from '~/types'
+
+export const defaultFavoriteGenres: GenreID[] = ['federal', 'kids', 'movies']
+export const defaultBrandColor: UnoCSSColorName = 'orange'
+export const defaultIsOpenNewTab = true
+export const defaultIsShowChannelsImages = true
+export const defaultIsShowPrograms = true
+export const defaultIsCompactChannels = false
+
+export const channelsListModesNames = {
+  list: 'Список',
+  compact: 'Компактный',
+  logos: 'Логотипы',
+}
+
+export type ChannelsListMode = keyof typeof channelsListModesNames
+
+export const defaultChannelsListMode: ChannelsListMode = 'list'
+
+export const defaultChannelsImagesSize = {
+  min: 48,
+  max: 1920,
+  step: 1,
+  default: 240,
+}
+
+export const defaultIsShowInfoOnHover = false
+
+export const defaultTvKeyboardDebounce = {
+  min: 1,
+  max: 10,
+  step: 1,
+  default: 5,
+}
+
+export const defaultTvKeyboardHideTime = {
+  min: 1,
+  max: 10,
+  step: 1,
+  default: 3,
+}
+
+export const defaultIsRealtimePrograms = false
+
+export const settingsDefaults = {
+  brandColor: defaultBrandColor,
+  channelsImagesSize: defaultChannelsImagesSize,
+  channelsListMode: defaultChannelsListMode,
+  favoriteGenres: defaultFavoriteGenres,
+  isOpenNewTab: defaultIsOpenNewTab,
+  isRealtimePrograms: defaultIsRealtimePrograms,
+  isShowChannelsImages: defaultIsShowChannelsImages,
+  isShowInfoOnHover: defaultIsShowInfoOnHover,
+  isShowPrograms: defaultIsShowPrograms,
+  tvKeyboardDebounce: defaultTvKeyboardDebounce,
+  tvKeyboardHideTime: defaultTvKeyboardHideTime,
+}
